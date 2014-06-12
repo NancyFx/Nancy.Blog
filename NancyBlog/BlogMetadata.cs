1e6ac84f60767dc7127069469152e243b2c9909b
Kristian Hellang <kristian@hellang.com>
2014-06-12 22:51:43 +0200

    Minor refactoring, added MD5 calculation for Gravatar

---
+++ NancyBlog/BlogMetadata.cs
@@ -0,0 +1,11 @@
+﻿namespace NancyBlog
+{
+    public class BlogMetadata
+    {
+        public string FeedUrl { get; set; }
+
+        public Author Author { get; set; }
+
+        public string Id { get; set; }
+    }
+}
