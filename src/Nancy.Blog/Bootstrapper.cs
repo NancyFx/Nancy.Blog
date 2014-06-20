04bfc3aca626b140176cf5817521dadc2558990d
Steven Robbins <ste.robbins@gmail.com>
2014-06-20 08:54:47 +0100

    Enabled error traces for the time being

--- src/Nancy.Blog/Bootstrapper.cs
+++ src/Nancy.Blog/Bootstrapper.cs
@@ -1,11 +1,19 @@
 ﻿namespace Nancy.Blog
 {
     using Nancy;
+    using Nancy.Bootstrapper;
     using Nancy.TinyIoc;
     using Services;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            StaticConfiguration.DisableErrorTraces = false;
+        }
+
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
         {
             container.Register<IConfigSettings, ConfigSettings>();
